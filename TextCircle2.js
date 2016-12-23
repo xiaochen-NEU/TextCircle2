c9db7c9e62ff77300cec3748a30c3de900a244d1
Xiao <chen.xiao3@husky.neu.edu>
2016-12-23 13:01:59 -0800

    interactive online HTML editing environment

--- TextCircle2.js
+++ TextCircle2.js
@@ -1,40 +1,42 @@
 this.Documents = new Mongo.Collection("documents");
 
-var myVar = 10;
-
 if (Meteor.isClient) {
 
-  // update the session current_date
-  // variable every 1000 ms
-  Meteor.setInterval(function(){
-    Session.set("current_date", new Date());
-  }, 1000);
+  // // update the session current_date
+  // // variable every 1000 ms
+  // Meteor.setInterval(function(){
+  //   Session.set("current_date", new Date());
+  // }, 1000);
 
-  Template.date_display.helpers({
-    current_date:function(){
-      return Session.get("current_date");
-    },
-    myVar:function(){
-      return myVar;
-    }
-  });
+  // Template.date_display.helpers({
+  //   current_date:function(){
+  //     return Session.get("current_date");
+  //   }
+  // });
 
   Template.editor.helpers({
     docid:function(){
       // console.log("doc id helper:");
       // console.log(Documents.findOne());
       // return Documents.findOne()._id;
-      var doc = Documents.findOne()
+      var doc = Documents.findOne();
       if (doc){
         return doc._id;
       }
       else {
         return undefined;
       }
-      
-    }
+    },
+    config:function(){
+      return function(editor){
+        editor.on("change", function(cm_editor, info){
+          console.log(cm_editor.getValue());
+          $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+        });
+
+      }
+    },
   });
-  
 }
 
 if (Meteor.isServer) {
