88069c27033696186aebd3d7be6a58ee830281cb
Xiao <chen.xiao3@husky.neu.edu>
2016-12-28 14:41:08 -0800

    Add user accounts and methods (interaction with DB)

--- TextCircle2.js
+++ TextCircle2.js
@@ -1,4 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
+EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
 
@@ -29,21 +30,82 @@ if (Meteor.isClient) {
     },
     config:function(){
       return function(editor){
+        editor.setOption("lineNumbers", true);
+        editor.setOption("theme", "cobalt");
+        editor.setOption("mode", "html");
         editor.on("change", function(cm_editor, info){
-          console.log(cm_editor.getValue());
+          //console.log(cm_editor.getValue());
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+          Meteor.call("addEditingUser");
         });
 
       }
     },
   });
-}
+
+  Template.editingUsers.helpers({
+    users:function(){
+      var doc, eusers, users;
+      doc = Documents.findOne();
+      if (!doc){return;} // give up
+      eusers = EditingUsers.findOne({docid:doc._id});
+      if (!eusers){return;} // give up
+      users = new Array();
+      var i = 0;
+      for (var user_id in eusers.users){
+        users[i] = fixObjectKeys(eusers.users[user_id]);
+        i++;
+
+      }
+      return users;
+
+    }
+  })
+
+}// end isClient...
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    // startup code that creates a document in case there isn't one yet. 
     if (!Documents.findOne()){// no documents yet!
       Documents.insert({title:"my new document"});
     }
   });
+}
+
+
+Meteor.methods({
+  addEditingUser: function(){
+    var doc, user, eusers;
+    doc = Documents.findOne();
+    if (!doc){return;} // no doc give up
+    if (!this.userId){return;} // no login user give up
+    // now I have a doc and possibly a user
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({docid:doc._id});
+    if (!eusers){
+      eusers = {
+        docid:doc._id,
+        users:{},
+      };
+    }
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    //upsert: insert or update if filter matches
+    EditingUsers.upsert({_id:eusers._id}, eusers); 
+  }
+})
+
+
+
+// this renames object keys by removing hyphens to make the compatible 
+// with spacebars. 
+function fixObjectKeys(obj){
+  var newObj = {};
+  for (key in obj){
+    var key2 = key.replace("-", "");
+    newObj[key2] = obj[key];
+  }
+  return newObj;
 }
